458fbb0a6c5a071719ea9b73cf3ab354a29225f3
Michael <maplambda@gmail.com>
2013-04-01 22:49:48 +1300

    etric info

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,21 @@
 import re
 import urlparse
+import json
 
 from hackergists import thriftdb
 from hackergists import model
+from hackergists import metrics
 from hackergists import gist as gist_api
+from hackergists import friendly_age
 
-criteria = dict(limit = 100,
-    sortby = 'create_ts desc',
-    q = 'gist.github.com',
-    filter = {'[fields][type]' : 'comment'})
+criteria = dict(limit = 100, sortby = 'create_ts desc',q = 'gist.github.com',
+                filter = {'[fields][type]' : 'comment'})
 
 thrift = thriftdb.ThriftDb('http://api.thriftdb.com', 'api.hnsearch.com', 'items')
 
-def load_gists():
-    notifications = []
-
+# returns gist id, hn discussion id
+def fetch_gist_ids():
+    ids = []
     comments = thrift.search(**criteria)
     nondead = [item for item in comments['results'] if item['item']['discussion'] is not None]
 
@@ -25,34 +26,57 @@ def load_gists():
             gist = {}
             path = urlparse.urlsplit(url.rstrip('/)')).path
             document = path.split('/').pop()
+            ids.append([document, discussion_id])
+    return ids
 
-            if model.exists(document):
-                continue
+def load_gists():
+    metrics.set_label('hackergists.lastupdate', int(friendly_age.get_universal_time()))
+    recent_gists = fetch_gist_ids()
+    
+    pipe = model.redis.pipeline()
+    for document, discussion_id in recent_gists:
+        pipe.exists(document)
 
+    key_exists = pipe.execute()
+    to_update = map(lambda x: x[0], filter(lambda x: x[1] == False, 
+                    zip(recent_gists, key_exists))) 
+
+    for document, discussion_id in to_update:
+        meta, error = gist_api.get(document)
+        if meta is None:
+            metrics.set_label(document, error)
+        else:
+            gist = {}
             gist['gist_id'] = document
             gist['discussion_id'] = discussion_id
-            meta = gist_api.get(document)
-            if meta is None:
-                notifications.append("Error getting gist data for: " + document)
-                continue
-
             gist['description'] = unicode(meta.description) if unicode(meta.description) else 'Gist: ' + document
             gist['url'] = meta.html_url
-            gist['content'] = '\n'.join(meta.files[meta.files.keys()[0]]\
-                .content.split('\n')[0:5])   
+            gist['content'] = unicode('\n'.join(meta.files[meta.files.keys()[0]].content.split('\n')[0:5]))
 
             model.store(gist, document)
-            model.index_add(id=document, score=discussion_id)
-
-    print "notifications: " + ','.join(notifications)
-    print "latest gist is: " + str(model.latest_gist())
-    print "recent gists: " + str(model.recent_gists())
+            model.index_add(score=discussion_id, id=document)
+            
+            metrics.clear_label(document)
 
 def kill_old():
-    #cause 400 ought to be enough
-    for key in model.index_clear_range(0, -401):
+    for key in model.index_clear_range(0, -501):
         model.delete(key)
 
 def flush_db():
-     model.flush()
-     model.index_clear()
+    model.flush()
+    model.index_clear()
+
+def info():
+    stat = {}
+    stat['updated_time'] = metrics.get_label('hackergists.lastupdate')
+    stat['errors'] = metrics.get_labels()
+    stat['redis_info'] = model.info()
+
+    print json.dumps(stat)
+    print "errors ", len(stat['errors'])
+    print "keys ", stat['redis_info']['db0']['keys']
+    print "last update", stat['updated_time']
+
+def updated():
+    print friendly_age.friendly_age(int(metrics.get_label('hackergists.lastupdate')))
+
